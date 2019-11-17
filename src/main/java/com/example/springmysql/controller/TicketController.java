bf120e25bfe4e1190012c9b6e7580e99915d6017
TrishaChetani <agarwalatrisha1212@gmail.com>
2019-11-18 00:04:08 +0100

    Modified CROS

--- src/main/java/com/example/springmysql/controller/TicketController.java
+++ src/main/java/com/example/springmysql/controller/TicketController.java
@@ -3,6 +3,7 @@ package com.example.springmysql.controller;
 import com.example.springmysql.dao.TicketDao;
 import com.example.springmysql.model.Ticket;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600,allowedHeaders =("*"),exposedHeaders = ("*") )
 @RestController
 @RequestMapping("/ticket")
 public class TicketController {
@@ -18,27 +19,16 @@ public class TicketController {
     private TicketDao dao;
 
     @PostMapping("/bookTickets")
-    @CrossOrigin()
+    @CrossOrigin(origins = "*", maxAge = 3600,allowedHeaders =("*"),exposedHeaders = ("*"))
     public String bookTicket(@RequestBody List<Ticket> ticket){
         dao.saveAll(ticket);
         return "ticket booked :" + ticket.size();
     }
 
     @GetMapping("/getTickets")
-    @CrossOrigin()
+    @CrossOrigin(origins = "*", maxAge = 3600,allowedHeaders =("*"),exposedHeaders = ("*"))
     public List <Ticket> getTickets(){
         return (List<Ticket>) dao.findAll();
-    }
-
-
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS","HEAD","PATCH").allowedOrigins("*")
-                        .allowedHeaders("*").exposedHeaders("*");
-            }
-        };
     }
 }
 
