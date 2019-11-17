68737aa6c23c9dd8b433a0d9d5e5366a151e8f11
TrishaChetani <agarwalatrisha1212@gmail.com>
2019-11-17 22:08:50 +0100

    Added CROS

--- src/main/java/com/example/springmysql/controller/TicketController.java
+++ src/main/java/com/example/springmysql/controller/TicketController.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/ticket")
 public class TicketController {
@@ -17,20 +18,25 @@ public class TicketController {
     private TicketDao dao;
 
     @PostMapping("/bookTickets")
+    @CrossOrigin()
     public String bookTicket(@RequestBody List<Ticket> ticket){
         dao.saveAll(ticket);
         return "ticket booked :" + ticket.size();
     }
 
     @GetMapping("/getTickets")
+    @CrossOrigin()
     public List <Ticket> getTickets(){
         return (List<Ticket>) dao.findAll();
     }
+
+
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/getTickets").allowedOrigins("http://localhost:9000");
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS","HEAD","PATCH").allowedOrigins("*")
+                        .allowedHeaders("*").exposedHeaders("*").;
             }
         };
     }
