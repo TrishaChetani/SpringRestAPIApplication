6cbb59b13ef0b01d19c98dd813995387227dcddf
TrishaChetani <agarwalatrisha1212@gmail.com>
2019-11-20 22:07:54 +0100

    Added mock bean

--- src/test/java/com/example/springmysql/controller/TicketControllerTest.java
+++ src/test/java/com/example/springmysql/controller/TicketControllerTest.java
@@ -3,11 +3,12 @@ package com.example.springmysql.controller;
 import com.example.springmysql.dao.TicketRepository;
 import com.example.springmysql.model.Ticket;
 import com.example.springmysql.service.TicketService;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
@@ -17,21 +18,13 @@ import static org.mockito.Mockito.when;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.*;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
+
 public class TicketControllerTest {
     @Autowired
     private TicketService ticketService;
-    @Autowired
+    @MockBean
     private TicketRepository repository;
 
 
@@ -56,7 +49,7 @@ public class TicketControllerTest {
     public void findTicketByAddressTest() {
         String address = "comments";
         when(repository.findByAddress(address))
-                .thenReturn(Stream.of(new Test(1, 31, "test")).collect(Collectors.toList()));
+        .thenReturn(Stream.of(new Ticket(1, 31, "test")).collect(Collectors.toList()));
         assertEquals(1, ticketService.getTicketByAddress(address).size());
     }
 
